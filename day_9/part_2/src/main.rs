fad0838499965c77746cf1bc41a503f310c80bb3
BigBlue <shenpaiwilliam@gmail.com>
2021-05-09 22:52:36 -0600

    Day 9

---
+++ day_9/part_2/src/main.rs
@@ -0,0 +1,90 @@
+use std::fs;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+
+fn main() {
+    // This is literally "copy pasta" coding when it comes to parse
+    // the input file as it has literally been the same for the last
+    // 8 days... but I want each problem to be independent of each other.
+    let f = fs::File::open("XMAS.txt").expect("Unable to open file");
+    let f = BufReader::new(f);
+    let mut iter = f.lines();
+
+    let mut input: Vec<u64> = Vec::new();
+
+    loop {
+        match iter.next() {
+            Some(val) => {
+                let curr_line = val.unwrap();
+                if curr_line == "" {
+                    break;
+                }
+                let value = curr_line.parse::<u64>().unwrap();
+                input.push(value);
+            },
+            None => {
+                break;
+            }
+        }
+    }
+
+    let start_index: usize = 25;
+    let mut key_number: u64 = 0;
+
+    for i in start_index..input.len() {
+        let prev = &input[i-start_index..i];
+        //assert!(prev.len() == 25);
+        let value = input[i];
+        if !contains_sum(value, prev) {
+            key_number = value;
+            break;
+        }
+    }
+
+    let mut number_range: Vec<u64> = Vec::new();
+    let mut sum: u64 = 0;
+
+    for i in 0..input.len() {
+        for j in i..input.len() {
+            let curr_value = input[j];
+            sum += curr_value;
+            number_range.push(curr_value);
+            if number_range.len() >= 2 {
+                if sum > key_number {
+                    sum = 0;
+                    number_range = Vec::new();
+                    break;
+                }
+                if j == (input.len() - 1) {
+                    sum = 0;
+                    number_range = Vec::new();
+                }
+                if sum == key_number {
+                    number_range.sort();
+                    let sum = number_range[0] + number_range[number_range.len()-1];
+                    println!("Sum: {}", sum);
+                    return;
+                }
+            }
+        }
+    }    
+}
+
+fn contains_sum(val: u64, entries: &[u64]) -> bool {
+    let set: HashSet<u64> = HashSet::from_iter(entries.iter().cloned());
+    for i in entries.iter() {
+        if val >= *i {
+            let diff = val - i;
+            // filter out same value pair
+            if diff != val {
+                if set.contains(&diff) {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
