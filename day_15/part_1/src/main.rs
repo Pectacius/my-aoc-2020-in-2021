70097cc0264d26b3aa015f3cf6af467fd17d0979
BigBlue <shenpaiwilliam@gmail.com>
2021-05-13 23:37:13 -0600

    Day 15 Part 1 - Incomplete Part 2

---
+++ day_15/part_1/src/main.rs
@@ -0,0 +1,23 @@
+use input_parser;
+
+mod game;
+
+fn main() {
+    let result = input_parser::read_line_input(15, "numbers".to_string());
+    let mut g = game::Game::new();
+
+    match result {
+        Some(value) => {
+            let starting_nums = value[0].split(",");
+            for i in starting_nums {
+                let num: u32 = i.parse().unwrap();
+                g.insert_starting_num(num);
+            }
+            let n = g.nth_number(2020);
+            println!("Number is: {}", n);
+        },
+        None => {
+            println!("Could not parse input");
+        }
+    }
+}
---
+++ day_15/part_2/src/game.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+
+pub struct Game {
+    last_nums: HashMap<u32, u32>, // number and turn last said
+    last_last_nums: HashMap<u32, u32>, // number and turn last last said 
+    last_said_num: u32,
+    turn: u32,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Game {
+            last_nums: HashMap::new(),
+            last_last_nums: HashMap::new(),
+            turn: 1,
+            last_said_num: 0,
+        }
+    }
+
+    pub fn insert_starting_num(&mut self, num: u32) {
+        self.last_nums.insert(num, self.turn);
+        self.last_said_num = num;
+        self.turn += 1;
+    }
+
+    fn insert_num(&mut self, num: u32) {
+        // essentially copy the values to last last if the value is already in last
+        match self.last_nums.get(&num) {
+            Some(val) => {
+                self.last_last_nums.insert(num, *val);
+            }
+            None => {}
+        }
+        self.last_nums.insert(num, self.turn);
+        self.last_said_num = num;
+    }
+
+    pub fn next_turn(&mut self) {
+        // check last number spoken
+        match self.last_last_nums.get(&self.last_said_num) {
+            // has been said before
+            Some(value) => {
+                let diff = self.last_nums.get(&self.last_said_num).unwrap() - value;
+                self.insert_num(diff);
+            },
+            // never said before
+            None => {
+                self.insert_num(0);
+            }
+        } 
+        self.turn += 1;
+    }
+
+    pub fn nth_number(&mut self, n: u32) -> u32 {
+        while self.turn <= n {
+            self.next_turn();
+            println!("{}", self.last_said_num);
+        }
+        self.last_said_num
+    }
+}
